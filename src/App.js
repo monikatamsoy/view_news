e3418ac8bdb5aa2ed296f2bc8be846738ddfaeb7
monikatamsoy <tamsoymonika@gmail.com>
2018-04-22 10:17:40 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import CustomNavbar from './components/navbar'
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+        <CustomNavbar />
+      
+      </div>
+    );
+  }
+}
+
+export default App;
